cb40556692a5289a3d6f2c72071ff369623310e0
richardsochima <richardsochima21@gmail.com>
2024-01-03 17:38:38 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
-        else:
-            print('{}{}, '.format(x, y), end='')
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}".format(str(i)+str(j)), end='\n'
+              if i == 8 and j == 9 else ', ')
